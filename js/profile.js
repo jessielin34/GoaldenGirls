9d848e85a5e09d71b64d74c791d10cb334dca382
Ishika Soni <ishikasoni50@gmail.com>
2024-03-13 19:08:58 -0400

    new goal creates a dropdown menu to create & join goal

--- js/profile.js
+++ js/profile.js
@@ -9,7 +9,7 @@ background.addEventListener("mouseout", e =>{
     edit.style.visibility = "hidden";
 });
 
-/* Edit background - can choose a pic but isnt getting replaced */
+/* Edit background - replace background 
 $(switchBackground);
 var oFReader = new FileReader(),
     rFilter = /^(?:image\/bmp|image\/cis\-cod|image\/gif|image\/ief|image\/jpeg|image\/jpeg|image\/jpeg|image\/pipeg|image\/png|image\/svg\+xml|image\/tiff|image\/x\-cmu\-raster|image\/x\-cmx|image\/x\-icon|image\/x\-portable\-anymap|image\/x\-portable\-bitmap|image\/x\-portable\-graymap|image\/x\-portable\-pixmap|image\/x\-rgb|image\/x\-xbitmap|image\/x\-xpixmap|image\/x\-xwindowdump)$/i;
@@ -31,4 +31,16 @@ function loadImageFile(testEl) {
     var oFile = testEl.files[0];
     if (!rFilter.test(oFile.type)) { alert("You must select a valid image file!"); return; }
     oFReader.readAsDataURL(oFile);
+}
+*/
+function readURL(input) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+
+        reader.onload = function (e) {
+            document.getElementById('img-bg').src = e.target.result;
+        }
+
+        reader.readAsDataURL(input.files[0]);
+    }
 }
