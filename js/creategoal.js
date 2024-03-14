b1bf027a19fc40ddaf385b112db09251cc4e2ddf
Ishika Soni <ishikasoni50@gmail.com>
2024-03-14 18:51:41 -0400

    goal creation form done - have to fix add cp later

--- js/creategoal.js
+++ js/creategoal.js
@@ -1,8 +1,10 @@
 
 document.getElementById("addCP").onclick = function() {
     var form = document.getElementById("goal-form");
-    var input = document.createElement("textarea");
+    var input = document.createElement("input");
     input.type = "text";
+    input.class = "form - control";
+    input.style = "border:solid";
     var br = document.createElement("br");
     form.appendChild(input);
     form.appendChild(br);
