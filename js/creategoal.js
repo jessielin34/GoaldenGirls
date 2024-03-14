a608278e584109e6ac82a8911f704a98fcc02d43
Ishika Soni <ishikasoni50@gmail.com>
2024-03-14 19:41:42 -0400

    final changes

--- js/creategoal.js
+++ js/creategoal.js
@@ -3,7 +3,7 @@ document.getElementById("addCP").onclick = function() {
     var form = document.getElementById("goal-form");
     var input = document.createElement("input");
     input.type = "text";
-    input.class = "form - control";
+    input.class = "form-control";
     input.style = "border:solid";
     var br = document.createElement("br");
     form.appendChild(input);
