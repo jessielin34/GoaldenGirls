3d45e0e3a016427ffaa7cc9a1efadb36505e47b4
Gabrie5a <80426217+Gabrie5a@users.noreply.github.com>
2024-03-18 19:34:33 -0400

    new file addGoal.js

---
+++ backend/addGoal.js
@@ -0,0 +1,45 @@
+const supabaseKey = 
+'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1waGViaXJ6eWRlb2dreWprdGpyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTA2MDY2OTksImV4cCI6MjAyNjE4MjY5OX0.qDac2fiFpZVq-TOfqIDfj9osvHOWpCBb6VIVgypSiMM';
+
+const supabaseUrl = 'https://mphebirzydeogkyjktjr.supabase.co';
+
+const _supabase = supabase.createClient(supabaseUrl, supabaseKey);
+console.log(supabase);
+
+let doneButton = document.querySelector("#done");
+doneButton.addEventListener("click", async(e)=>{
+    e.preventDefault();
+    let name = document.querySelector("#goal-title").value;
+    let description = document.querySelector("#goal-description").value;
+    let checkpoint1 = document.querySelector("#checkpoint1").value;
+    let checkpoint2 = document.querySelector("#checkpoint2").value;
+    let checkpoint3 = document.querySelector("#checkpoint3").value;
+    if(name != "" && description != "" && checkpoint1 != ""){
+        const {data, error} = await _supabase.from("Goals").insert({
+            goal: name,
+            user_id: auth.uid(),
+            description: description,
+        });
+        console.log(data);
+        // get id of goal by checking auth.uid() && 
+        // https://www.youtube.com/watch?v=roAJ61sTGIc
+        // https://supabase.com/docs/guides/auth/managing-user-data
+        const res = await _supabase.from("Checkpoint").insert({
+            goal_id: goalID
+        });
+        console.log(res);
+        alert("Your goal has been added!");
+        alert("Your goal has been added!");
+
+    }
+    else{
+        console.log("Unable to add to database");
+        alert("Unable to add goal ;( \nMake sure to fill out all fields and at least 1 checkpoint!");
+    }
+    
+});
+
+
+function addGoal (goalText){
+
+}
